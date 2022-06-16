304015885c64e20bf42ba887764d6422e5fa0760
Sainikhi1506 <89088675+Sainikhi1506@users.noreply.github.com>
2022-06-16 18:26:01 +0530

    Create arth.java

---
+++ arth.java
@@ -0,0 +1,20 @@
+class x1
+{
+  public static void main(String args[])
+  {
+    int a,b,c;
+    a=10;
+    b=30;
+    c=a+b;
+    //Addition of two number
+    System.out.println(c);
+    
+    //Subtraction
+    c=a-b;
+    System.out.println(c);
+    
+    //Multiplication
+    c=a*b;
+    System.out.println(c);
+  }
+}
